a979aa8d86ac9a8017da65c684498b56dce44c75
CauaHvS <cauahenrique230503@gmail.com>
2022-03-16 10:48:43 -0300

    Second commit

---
+++ AEDIII/WriteDoubleData.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.*;
+
+
+public class WriteDoubleData {
+    public static void main(String[] args) throws IOException {
+        double[] data = {1.555550,3.58,6.78897,1.333333,2.8745};
+        double[] aux;
+
+        writeData(data, "file.txt");
+
+    }
+    public static void writeData(double data[], String nome_arq) throws IOException{
+        FileOutputStream fout = new FileOutputStream(nome_arq);
+        DataOutputStream out = new DataOutputStream(fout);
+
+        out.writeInt(data.length);
+
+        for (int i =0; i< data.length;i++){
+            out.writeDouble(data[i]);
+
+        }
+        out.close();
+    }
+
+
+
+}
+
